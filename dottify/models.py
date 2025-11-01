ae23acc60c493ad384f06874a2d0d5c0f1c7f5ec
TamaraSovcikova <tamara@sovcik.net>
2025-11-01 12:06:23 +0000

    completed and refined the album model to match criteria

--- dottify/models.py
+++ dottify/models.py
@@ -1,12 +1,11 @@
 from django.conf import settings
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.template.defaultfilters import slugify
 from django.utils import timezone
 from datetime import timedelta
 
-# Create your models here.
-
-
+#TODO: User is week 4 material - make sure to go over it again
 class DottifyUser(models.Model):
     # Links to the built-in Django User model
     user = models.OneToOneField(
@@ -15,24 +14,20 @@ class DottifyUser(models.Model):
         related_name='dottify_profile' 
     )
 
-    display_name = models.CharField(
-        max_length=800, # need to double check the requirement for this later
-        unique=True,
-        blank=False,
-        null=False
-    )
+    display_name = models.CharField(max_length=800,unique=True, null=False, blank=False)
 
     def __str__(self):
         return self.display_name
 
-def get_max_release_date():
-    # 6 * 30 days is 180 days. Using timedelta is safest.
-    return timezone.now().date() + timedelta(days=6*30)
+# Helper function for Albums release date calculation
+def get_max_release_date():    
+    # Calculates the date 60*3 180 days (6 months) from today, inclusive
+    return timezone.now().date() + timedelta(days=6 * 30)
 
 
 class Album(models.Model):
     
-    # --- 1. Choices for Format ---
+    # --- Choices for Format ---
     class Format(models.TextChoices):
         SINGLE = 'SNGL', 'Single'
         REMASTER = 'RMST', 'Remaster'
@@ -40,67 +35,41 @@ class Album(models.Model):
         COMPILATION = 'COMP', 'Compilation'
         LIVE_RECORDING = 'LIVE', 'Live Recording'
 
-
-    # --- 2. Fields & Constraints ---
-
-    # Optional image, uses the default 'no_cover.jpg'. No upload_to set
-    cover_image = models.ImageField(
-        default='no_cover.jpg',
-        blank=True,
-        null=True
-    )
-
-    title = models.CharField(
-        max_length=800,
-        blank=False,
-        null=False
-    )
-
-    artist_name = models.CharField(
-        max_length=800,
-        blank=False,
-        null=False
-    )
+    cover_image = models.ImageField(default='no_cover.jpg', blank=True, null=True) #blank=True gives the option of empty fields
+    title = models.CharField(max_length=800)
+    artist_name = models.CharField(max_length=800)
     
-    # Optional link to a DottifyUser (ForeignKey, allows null)
+    # Optional link 
     artist_account = models.ForeignKey(
-        'DottifyUser', # String reference is safer for now
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name='albums' # for reverse lookup
+        DottifyUser, 
+        on_delete=models.SET_NULL, # If the user is deleted, keep the album but set account to NULL
+        null=True, 
+        blank=True
     )
-
-    format = models.CharField(
-        max_length=4,
-        choices=Format.choices,
-        blank=True,
-        null=True
-    )
-
     retail_price = models.DecimalField(
         max_digits=5, # Need 3 digits before the decimal, 2 after (3+2=5)
         decimal_places=2,
         validators=[MinValueValidator(0.00), MaxValueValidator(999.99)],
-        blank=False,
-        null=False
+        null=False, 
+        blank=False
     )
-    
-    # Release date: required, must be in the past or max 6*30 days in the future
-    release_date = models.DateField(
-        validators=[MaxValueValidator(get_max_release_date)],
-        blank=False,
-        null=False
-    )
-
-    # Slug field: not unique, but required on save. Must be blank/null on instantiation.
-    # TODO : Implement said field
+    format = models.CharField(max_length=4, choices=Format.choices, blank=True, null=True)
+    release_date = models.DateField(validators=[MaxValueValidator(limit_value=get_max_release_date)], null=False, blank=False) #TODO: double check if this is correct format
+    slug = models.SlugField(null=True, blank=True)
   
-    class Meta:        
-        unique_together = ['title', 'artist_name', 'format']
-        verbose_name_plural = "Albums"
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'artist_name', 'format'],
+                name='unique_album_by_artist_and_format'
+            )
+        ]
+    
+    def save(self, *args, **kwargs):
+        # Dynamic slugs are updateable
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
-    # TODO: implement save method
-
+# returns a formatted string combiting the title and artist's name
     def __str__(self):
         return f"{self.title} by {self.artist_name}"
